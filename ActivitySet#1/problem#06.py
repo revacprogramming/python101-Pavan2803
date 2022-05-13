ae8affebb4ef60a66074ccecc8c2b3e47034e6d9
Pavankumar Gowda <93110556+Pavan2803@users.noreply.github.com>
2022-05-13 09:53:05 +0000

    Apple

--- ActivitySet#1/problem#06.py
+++ ActivitySet#1/problem#06.py
@@ -4,13 +4,22 @@ largest = None
 smallest = None
 
 while True:
-    num = input("Enter a number? ")
+    inp = input("Enter a number: ")
+    if inp == "done" : break
+    try:
+        num = float(inp)
+    except:
+        print ("Invalid input")
+        continue
+    if smallest is None:
+        smallest = num 
+    if num > largest :
+        largest = num
+    elif num < smallest :
+        smallest = num
 
-    if num == "done":
-        break
+def done(largest,smallest):
+    print ("Maximum is", int(largest))  
+    print ("Minimum is", int(smallest))
 
-    # ...
-
-    print(num)
-
-print("Maximum", largest)
+done(largest,smallest)
